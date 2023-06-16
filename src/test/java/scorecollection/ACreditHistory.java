6fc984855c333ed4e3e7aab2e7670949465418b1
Jeff Langr <jeff@langrsoft.com>
2023-06-15 21:51:52 -0600

    callouts

--- src/test/java/scorecollection/ACreditHistory.java
+++ src/test/java/scorecollection/ACreditHistory.java
@@ -2,16 +2,16 @@ package scorecollection;
 
 import org.junit.jupiter.api.Test;
 // START_HIGHLIGHT
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals; // <callout id="co.import"
 // END_HIGHLIGHT
 
 public class ACreditHistory {
    // START_HIGHLIGHT
    @Test
-   void withNoCreditRatingsHasAMeanOf0() {
-      var creditHistory = new CreditHistory();
-      var result = creditHistory.arithmeticMean();
-      assertEquals(0, result);
+   void withNoCreditRatingsHasAMeanOf0() { // <callout id="co.name"
+      var creditHistory = new CreditHistory(); // <callout id="co.arrange"
+      int result = creditHistory.arithmeticMean(); // <callout id="co.act"
+      assertEquals(0, result); // <callout id="co.assert"
    }
    // END_HIGHLIGHT
 }
