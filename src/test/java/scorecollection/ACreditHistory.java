aa51648bedf1610a946fb04ac80de8f27ee00bef
Jeff Langr <jeff@langrsoft.com>
2023-06-15 21:19:38 -0600

    first real test

--- src/test/java/scorecollection/ACreditHistory.java
+++ src/test/java/scorecollection/ACreditHistory.java
@@ -1,11 +1,17 @@
 package scorecollection;
 
 import org.junit.jupiter.api.Test;
+// START_HIGHLIGHT
+import static org.junit.jupiter.api.Assertions.assertEquals;
+// END_HIGHLIGHT
 
 public class ACreditHistory {
    // START_HIGHLIGHT
    @Test
-   void whatever() {
+   void withNoCreditRatingsHasAMeanOf0() {
+      var creditHistory = new CreditHistory();
+      var result = creditHistory.arithmeticMean();
+      assertEquals(0, result);
    }
    // END_HIGHLIGHT
 }
