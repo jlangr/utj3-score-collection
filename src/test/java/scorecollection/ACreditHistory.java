36e2ac72ad2cf3f8c4a686db130c2ab0cb65fb7d
Jeff Langr <jeff@langrsoft.com>
2023-06-19 21:28:27 -0600

    change from label to callout

--- src/test/java/scorecollection/ACreditHistory.java
+++ src/test/java/scorecollection/ACreditHistory.java
@@ -1,13 +1,13 @@
 package scorecollection;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals; // <callout id="co.import"/>
+import static org.junit.jupiter.api.Assertions.assertEquals; // <label id="co.import"/>
 
 public class ACreditHistory {
    @Test
-   void withNoCreditRatingsHasAMeanOf0() { // <callout id="co.name"/>
-      var creditHistory = new CreditHistory(); // <callout id="co.arrange"/>
-      int result = creditHistory.arithmeticMean(); // <callout id="co.act"/>
-      assertEquals(0, result); // <callout id="co.assert"/>
+   void withNoCreditRatingsHasAMeanOf0() { // <label id="co.name"/>
+      var creditHistory = new CreditHistory(); // <label id="co.arrange"/>
+      int result = creditHistory.arithmeticMean(); // <label id="co.act"/>
+      assertEquals(0, result); // <label id="co.assert"/>
    }
 }
