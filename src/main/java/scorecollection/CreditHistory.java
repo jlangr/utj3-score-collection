0ca3be1418a30694fd00cd5eb5387c41434e9e9d
Jeff Langr <jeff@langrsoft.com>
2023-06-26 20:35:50 -0600

    one-based test

--- src/main/java/scorecollection/CreditHistory.java
+++ src/main/java/scorecollection/CreditHistory.java
@@ -6,18 +6,14 @@ import java.util.List;
 public class CreditHistory {
    private final List<CreditRating> ratings = new ArrayList<>();
    
-   public void add(CreditRating scoreable) {
-      ratings.add(scoreable);
+   public void add(CreditRating rating) {
+      ratings.add(rating);
    }
 
-   // START:arithmeticMean
    public int arithmeticMean() {
-      // START_HIGHLIGHT
       if (ratings.size() == 0) return 0;
-      // END_HIGHLIGHT
 
       var total = ratings.stream().mapToInt(CreditRating::rating).sum();
       return total / ratings.size();
    }
-   // END:arithmeticMean
 }
--- src/test/java/scorecollection/ACreditHistory.java
+++ src/test/java/scorecollection/ACreditHistory.java
@@ -1,6 +1,9 @@
 package scorecollection;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ACreditHistory {
@@ -10,4 +13,14 @@ public class ACreditHistory {
       int result = creditHistory.arithmeticMean();
       assertEquals(0, result);
    }
+
+   // START:oneBasedTest
+   @Test
+   void withOneRatingHasEquivalentMean() {
+      var creditHistory = new CreditHistory();
+      creditHistory.add(new CreditRating(780, LocalDate.now()));
+      int result = creditHistory.arithmeticMean();
+      assertEquals(780, result);
+   }
+   // END:oneBasedTest
 }
