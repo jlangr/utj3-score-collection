96481a71d65082b5e52a53038806bec6d1611b14
Jeff Langr <jeff@langrsoft.com>
2023-06-28 21:45:31 -0600

    no class

--- src/test/java/scorecollection/ACreditHistory.java
+++ src/test/java/scorecollection/ACreditHistory.java
@@ -3,7 +3,7 @@ package scorecollection;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals; // <label id="co.import"/>
 
-public class ACreditHistory {
+class ACreditHistory {
    @Test
    void withNoCreditRatingsHas0Mean() { // <label id="co.name"/>
       var creditHistory = new CreditHistory(); // <label id="co.arrange"/>
