cd3ab4802526cd42fd95311a64ca299dc447b3f1
Jeff Langr <jeff@langrsoft.com>
2023-06-28 21:44:07 -0600

    no public

--- src/test/java/scorecollection/ACreditHistory.java
+++ src/test/java/scorecollection/ACreditHistory.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ACreditHistory {
+class ACreditHistory {
    @Test
    void withNoCreditRatingsHas0Mean() {
       var creditHistory = new CreditHistory();
