e07705cbf36faf4745c6e808c56d759932b1b5dd
Jeff Langr <jeff@langrsoft.com>
2023-06-28 20:40:34 -0600

    beforeeach

--- src/test/java/scorecollection/ACreditHistory.java
+++ src/test/java/scorecollection/ACreditHistory.java
@@ -1,32 +1,37 @@
 package scorecollection;
 
+// START_HIGHLIGHT
+import org.junit.jupiter.api.BeforeEach;
+// END_HIGHLIGHT
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// START:testClass
 public class ACreditHistory {
+   // START_HIGHLIGHT
+   CreditHistory creditHistory;
+   // END_HIGHLIGHT
+
+   // START_HIGHLIGHT
+   @BeforeEach
+   void createInstance() {
+      creditHistory = new CreditHistory();
+   }
+   // END_HIGHLIGHT
+
    @Test
    void withNoCreditRatingsHas0Mean() {
-      var creditHistory = new CreditHistory();
-
       int result = creditHistory.arithmeticMean();
 
       assertEquals(0, result);
    }
 
-   // START:scannability
    @Test
    void withOneRatingHasEquivalentMean() {
-      // START:arrange
-      var creditHistory = new CreditHistory();
-      // END:arrange
       creditHistory.add(new CreditRating(780, LocalDate.now()));
 
       int result = creditHistory.arithmeticMean();
 
       assertEquals(780, result);
    }
-   // END:scannability
 }
-// END:testClass
