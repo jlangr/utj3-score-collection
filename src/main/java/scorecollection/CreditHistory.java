20e0d68ceeace4fa549709afa6153e5f9fa8de86
Jeff Langr <jeff@langrsoft.com>
2023-10-17 21:12:18 -0600

    hmmm

--- src/main/java/scorecollection/CreditHistory.java
+++ src/main/java/scorecollection/CreditHistory.java
@@ -18,20 +18,14 @@ public class CreditHistory {
       ratings.add(new CreditRating(rating, LocalDate.now()));
    }
 
-   // START:overflow
    public int arithmeticMean() {
       if (ratings.size() == 0) throw new IllegalStateException();
 
       var total = ratings.stream()
-      // START_HIGHLIGHT
                      .mapToLong(CreditRating::rating)
-      // END_HIGHLIGHT
                      .sum();
-      // START_HIGHLIGHT
-      return (int)(total / ratings.size());
-      // END_HIGHLIGHT
+      return (int)total / ratings.size();
    }
-   // END:overflow
 
    public long daysSpanned() {
       if (ratings.isEmpty()) return 0;
