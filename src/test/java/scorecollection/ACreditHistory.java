df0c1e1940ba2003bd19df2aa9dd15730bb80e69
Jeff Langr <jeff@langrsoft.com>
2023-10-17 20:14:57 -0600

    add zero marker

--- src/test/java/scorecollection/ACreditHistory.java
+++ src/test/java/scorecollection/ACreditHistory.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ACreditHistory {
+   // START:zero
    @Test
    void withNoCreditRatingsHas0Mean() {
       var creditHistory = new CreditHistory();
       int result = creditHistory.arithmeticMean();
       assertEquals(0, result);
    }
+   // END:zero
 }
