e0d824a588994cd2a7a7318fc02986420da1a143
Jeff Langr <jeff@langrsoft.com>
2023-10-18 21:44:56 -0600

    newton

---
+++ src/test/java/scorecollection/utils/SomeMathUtils.java
@@ -0,0 +1,41 @@
+package scorecollection.utils;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static java.lang.Math.abs;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeMathUtils {
+   // START:NewtonClass
+   class Newton {
+      static final double TOLERANCE = 1E-16;
+
+      static double squareRoot(double n) {
+         double approx = n;
+         while (abs(approx - n / approx) > TOLERANCE * approx)
+            approx = (n / approx + approx) / 2.0;
+         return approx;
+      }
+   }
+   // END:NewtonClass
+
+   @Nested
+   class SquareRoot {
+      // START:verifiedWithMultiplication
+      @Test
+      public void verifiedWithMultiplication() {
+         var result = Newton.squareRoot(250.0);
+         assertEquals(250.0, result * result,
+            Newton.TOLERANCE);
+      }
+      // END:verifiedWithMultiplication
+
+      // START:verifiedUsingJavaLibrary
+      @Test
+      public void verifiedUsingJavaLibrary() {
+         assertEquals(Math.sqrt(1969.0), Newton.squareRoot(1969.0),
+            Newton.TOLERANCE);
+      }
+      // END:verifiedUsingJavaLibrary
+   }
+}
