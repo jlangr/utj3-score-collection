25c289f601048100d01227635b46d17847b6447e
Jeff Langr <jeff@langrsoft.com>
2023-10-20 13:58:20 -0600

    performance stuff in square root

--- src/test/java/scorecollection/utils/SomeMathUtils.java
+++ src/test/java/scorecollection/utils/SomeMathUtils.java
@@ -2,19 +2,24 @@ package scorecollection.utils;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
 import static java.lang.Math.abs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SomeMathUtils {
    // START:NewtonClass
-   class Newton {
-      static final double TOLERANCE = 1E-16;
+   public class Newton {
+      static final double ACCURACY = 1E-8;
 
-      static double squareRoot(double n) {
-         double approx = n;
-         while (abs(approx - n / approx) > TOLERANCE * approx)
-            approx = (n / approx + approx) / 2.0;
-         return approx;
+      public static double squareRoot(double n) {
+         double approximation = n / 2;
+         while (abs(approximation * approximation - n) > ACCURACY)
+            approximation = (approximation + n / approximation) / 2.0;
+         return approximation;
       }
    }
    // END:NewtonClass
@@ -23,19 +28,36 @@ public class SomeMathUtils {
    class SquareRoot {
       // START:verifiedWithInverse
       @Test
-      public void verifiedWithInverse() {
+      void verifiedWithInverse() {
          var result = Newton.squareRoot(250.0);
          assertEquals(250.0, result * result,
-            Newton.TOLERANCE);
+            Newton.ACCURACY);
       }
       // END:verifiedWithInverse
 
       // START:verifiedUsingJavaLibrary
       @Test
-      public void verifiedUsingJavaLibrary() {
+      void verifiedUsingJavaLibrary() {
          assertEquals(Math.sqrt(1969.0), Newton.squareRoot(1969.0),
-            Newton.TOLERANCE);
+            Newton.ACCURACY);
       }
       // END:verifiedUsingJavaLibrary
+
+      // START:run
+      long time(int times, Consumer<Integer> func) {
+         var start = System.nanoTime();
+         IntStream.range(0, times).forEach(i -> func.accept(i + 1));
+         return (System.nanoTime() - start) / 1_000_000;
+      }
+      // END:run
+
+      // START:performance
+      @Test
+      void executesInSufficientTime() {
+         var numberOfTimes = 100_000;
+         var elapsedMs = time(numberOfTimes, i -> Newton.squareRoot(i));
+         assertTrue(elapsedMs < 1000);
+      }
+      // END:performance
    }
 }
